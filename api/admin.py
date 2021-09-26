b112171b9ca249e21b9b28e87fc04f27a6514553
Rachel Kat <rachel.kat@koho.ca>
2021-09-26 15:53:47 -0400

    models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Account, Profile, Question, Answer, Scenario
 # Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Profile)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Scenario)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    email = models.EmailField(max_length=254)
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    level = models.PositiveSmallIntegerField(default=0)
+    net_worth = models.IntegerField(default=0)
+    stress_level = models.PositiveSmallIntegerField(default=0)
+    experience = models.PositiveSmallIntegerField(default=0)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    question = models.CharField(max_length=500)
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=500)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    impact = models.JSONField()
+
+class Scenario(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    response = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
