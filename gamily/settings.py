9cc2bf1aff5c6a1460b17d3e4f4dc299a30db6f0
Rachel Kat <rachelkat@gmail.com>
2021-10-16 23:35:34 -0400

    more trying

--- gamily/settings.py
+++ gamily/settings.py
@@ -27,7 +27,7 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
