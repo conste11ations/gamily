ba9d3fec2435e9d3c5b1fb1a2411ef78caeb9748
Rachel Kat <rachelkat@gmail.com>
2021-10-17 14:13:47 -0400

    trying to fix

--- gamily/settings.py
+++ gamily/settings.py
@@ -158,6 +158,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+       'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
